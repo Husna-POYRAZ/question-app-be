55f9f0ce5ddff597a39b184e667b01eceb7e69fb
Hüsna POYRAZ <75911392+Husna-POYRAZ@users.noreply.github.com>
2023-04-25 00:05:23 +0300

    first commit

---
+++ src/test/java/com/hpoyraz/questionappbe/QuestionAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpoyraz.questionappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
